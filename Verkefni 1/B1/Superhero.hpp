d15c58f9d89a277582fa82ccebedd633f812607e
hjordissveinsd <hjordissveinsdottir@Hjordiss-MacBook-Pro.local>
2017-11-27 14:24:08 +0000

    bætti við klasaskrám

---
+++ Verkefni 1/B1/Superhero.hpp
@@ -0,0 +1,14 @@
+//
+//  Superhero.hpp
+//  Verk1B1
+//
+//  Created by Hjördís Sveinsdóttir on 27/11/2017.
+//  Copyright © 2017 Hjördís Sveinsdóttir. All rights reserved.
+//
+
+#ifndef Superhero_hpp
+#define Superhero_hpp
+
+#include <stdio.h>
+
+#endif /* Superhero_hpp */
---
+++ Verkefni 1/B1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Verk1B1
+//
+//  Created by Hjördís Sveinsdóttir on 27/11/2017.
+//  Copyright © 2017 Hjördís Sveinsdóttir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
