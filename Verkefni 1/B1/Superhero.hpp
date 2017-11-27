d0daa5ba67eff0bc58b30e0c2f9097da1b871a0c
hjordissveinsd <hjordissveinsdottir@Hjordiss-MacBook-Pro.local>
2017-11-27 14:43:26 +0000

    breytti main

--- Verkefni 1/B1/Superhero.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef Superhero_hpp
-#define Superhero_hpp
-
-#include <stdio.h>
-
-#endif /* Superhero_hpp */
--- Verkefni 1/B1/include/superhero.h
+++ Verkefni 1/B1/include/superhero.h
@@ -6,7 +6,7 @@ class Superhero
 {
     public:
         Superhero();
-
+    
     protected:
 
     private:
