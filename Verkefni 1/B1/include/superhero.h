96f7448736f9ca7450573272c0768429af18c93c
Þorri Magnússon <thonnim@gmail.com>
2017-11-27 20:57:14 +0000

    Created two constructors and ostream and istream overloaders to the Superhero class

--- Verkefni 1/B1/include/superhero.h
+++ Verkefni 1/B1/include/superhero.h
@@ -1,17 +1,26 @@
 #ifndef SUPERHERO_H
 #define SUPERHERO_H
 
+#include <iostream>
 #include <string>
+
+using namespace std;
 
 class Superhero
 {
     public:
-        Superhero(std::string name, int age, char superpower);
+        Superhero();
+        Superhero(string name, int age, char superpower);
+
+        string getSuperpower() const;
+
+        friend ostream& operator <<(ostream &out, const Superhero &superhero);
+        friend istream& operator >>(istream &in, Superhero &superhero);
 
     protected:
 
     private:
-        std::string name;
+        string name;
         int age;
         char superpower;
 };
--- Verkefni 1/B1/src/superhero.cpp
+++ Verkefni 1/B1/src/superhero.cpp
@@ -1,6 +1,60 @@
 #include "Superhero.h"
 
+
+Superhero::Superhero()
+{
+    this->name = "";
+    this->age = 0;
+    this->superpower = 'n';
+}
+
+
 Superhero::Superhero(std::string name, int age, char superpower)
 {
-    //ctor
+    this->name = name;
+    this->age = age;
+    this->superpower = superpower;
+}
+
+
+string Superhero::getSuperpower() const
+{
+    if(this->superpower == 'f')
+    {
+        return "Flying";
+    }
+    else if(this->superpower == 'g')
+    {
+        return "Giant";
+    }
+    else if(this->superpower == 'h')
+    {
+        return "Hacker";
+    }
+    else if(this->superpower == 'n')
+    {
+        return "None";
+    }
+    else
+    {
+        return "Weakling";
+    }
+}
+
+
+ostream& operator <<(ostream &out, const Superhero &superhero)
+{
+    out << superhero.name << " (" << superhero.age << "): " << superhero.getSuperpower() << endl;
+
+    return out;
+}
+
+
+istream& operator >>(istream &in, Superhero &superhero)
+{
+    in >> superhero.name;
+    in >> superhero.age;
+    in >> superhero.superpower;
+
+    return in;
 }
