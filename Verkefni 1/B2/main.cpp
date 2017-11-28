7e93ffdc47a7233bfde240001ea820431dc72a5a
Sigudur Galdur <galdur97@hotmail.com>
2017-11-28 15:24:26 +0000

    Making shit happen

--- Verkefni 1/B2/main.cpp
+++ Verkefni 1/B2/main.cpp
@@ -17,8 +17,25 @@ int main()
     fout.write((char*)(&hero), sizeof(Superhero));
 
     hero.setVerbose(true);
+    fout.close();
 
+    ifstream fin;
 
+    fin.open("superherofile.dat", ios::binary);
+
+    fin.seekg(0, fin.end);
+    int records = fin.tellg() / sizeof(Superhero);
+    fin.seekg(0, fin.beg);
+
+    Superhero *hero1 = new Superhero[records];
+
+    fin.read((char*)hero1, sizeof(Superhero) * records);
+
+    fin.close();
+
+    for(int i = 0; i < records; i++) {
+        cout << hero1[i] << endl;
+    }
 
     return 0;
 }
