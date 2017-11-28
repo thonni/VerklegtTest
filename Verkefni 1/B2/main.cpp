9803d8e383276c3855c4c1f5cb2e0deee8bab889
Sigudur Galdur <galdur97@hotmail.com>
2017-11-28 14:44:42 +0000

    Copy-adi B1 yfir i B2

--- Verkefni 1/B2/main.cpp
+++ Verkefni 1/B2/main.cpp
@@ -1,9 +1,97 @@
 #include <iostream>
+#include <fstream>
+#include "include/superhero.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char addHero = 'n';
+
+    //Ask the user if he/she wants to add a hero.
+
+    do {
+        cout << "Add hero? (Y/N): ";
+        cin >> addHero;
+    } while (!(toupper(addHero) == 'N' || toupper(addHero) == 'Y'));
+
+    if(toupper(addHero) == 'Y')
+    {
+        string name = "";
+        int age = 0;
+        char superpower = 'n';
+
+        //Ask for the hero's name.
+        cout << "Name: ";
+        cin >> name;
+
+        //Ask for the hero's age.
+        cout << "Age: ";
+        cin >> age;
+
+        //Ask for the hero's superpower.
+        cout << "Please enter a superpower:" << endl;
+        cout << "Input f for Flying" << endl;
+        cout << "Input g for Giant" << endl;
+        cout << "Input h for Hacker" << endl;
+        cout << "Input n for None" << endl;
+        cout << "If other input, your superhero is a Weakling" << endl;
+        cout << "Superpower: ";
+        cin >> superpower;
+
+        //Create a new Superhero using the user input.
+        Superhero newHero(name, age, superpower);
+
+        //Create the ofstream file.
+        ofstream fout;
+        fout.open("superheroes.txt", ios::app);
+
+        //Save the hero information to text file.
+        newHero.setVerbose(false);
+        fout << newHero;
+        newHero.setVerbose(true);
+
+        //Close the file
+        fout.close();
+
+    }
+
+    //Create the ifstream file.
+    ifstream fin;
+    fin.open("superheroes.txt");
+
+    //Check if the file is open (Check if it exists).
+    if(fin.is_open())
+    {
+        string name;
+        int age;
+        char superpower;
+
+        //Loop until the file ends.
+        while(fin >> name && fin >> age && fin >> superpower)
+        {
+            //Fetch the data.
+
+            //Create a Superhero with the data
+            Superhero tempHero(name, age, superpower);
+
+            //Print the Superhero to the screen.
+            cout << tempHero << endl;
+        }
+
+        //Close the file.
+        fin.close();
+    }
+    else
+    {
+        //If the file didn't open, print an error to the screen.
+        cout << "ERROR: Could not open file" << endl;
+    }
+
+    //Stuff Thorri needs to do because the program closes the moment it ends.
+    cin >> addHero;
+
+
+
     return 0;
 }
---
+++ Verkefni 1/B2/src/superhero.cpp
@@ -0,0 +1,75 @@
+#include "../include/superhero.h"
+
+
+Superhero::Superhero()
+{
+    this->name = "";
+    this->age = 0;
+    this->superpower = 'n';
+    this->verbose = true;
+}
+
+
+Superhero::Superhero(string name, int age, char superpower)
+{
+    this->name = name;
+    this->age = age;
+    this->superpower = superpower;
+    this->verbose = true;
+}
+
+
+string Superhero::getSuperpower() const
+{
+    if(this->superpower == 'f')
+    {
+        return "Flying";
+    }
+    else if(this->superpower == 'g')
+    {
+        return "Giant";
+    }
+    else if(this->superpower == 'h')
+    {
+        return "Hacker";
+    }
+    else if(this->superpower == 'n')
+    {
+        return "None";
+    }
+    else
+    {
+        return "Weakling";
+    }
+}
+
+
+void Superhero::setVerbose(bool v)
+{
+    this->verbose = v;
+}
+
+
+ostream& operator <<(ostream &out, const Superhero &superhero)
+{
+    if(superhero.verbose)
+    {
+        out << superhero.name << " (" << superhero.age << "): " << superhero.getSuperpower() << endl;
+    }
+    else
+    {
+        out << superhero.name << " " << superhero.age << " " << superhero.superpower << endl;
+    }
+
+    return out;
+}
+
+
+istream& operator >>(istream &in, Superhero &superhero)
+{
+    in >> superhero.name;
+    in >> superhero.age;
+    in >> superhero.superpower;
+
+    return in;
+}
