757245491b68491b5b749b247891017f46f5096d
hjordissveinsd <hjordis16@ru.is>
2017-11-29 15:32:13 +0000

    bætti við MainUI

--- MainAssignment/MainAssignment/include/MainUI.h
+++ MainAssignment/MainAssignment/include/MainUI.h
@@ -1,6 +1,8 @@
 #ifndef MAINUI_H
 #define MAINUI_H
+#include <iostream>
 
+using namespace std;
 
 class MainUI
 {
--- MainAssignment/MainAssignment/src/MainUI.cpp
+++ MainAssignment/MainAssignment/src/MainUI.cpp
@@ -1,4 +1,7 @@
 #include "MainUI.h"
+#include <iostream>
+using namespace std;
+
 
 MainUI::MainUI()
 {
@@ -12,5 +15,31 @@ MainUI::~MainUI()
 
 void MainUI::startUI()
 {
+    cout << "Welcome to Pizza hut" << endl;
+    cout << "Please tell us who you are:" << endl;
+    cout << "Choose m for manager" << endl;
+    cout << "Choose s for salesman" << endl;
+    cout << "Choose b for baker" << endl;
+    cout << "Choose d for delivery" << endl;
 
+    char selection = '\0';
+
+    cin >> selection;
+
+    if (selection == 'm')
+    {
+
+    }
+    if (selection == 's')
+    {
+
+    }
+    if (selection == 'b')
+    {
+
+    }
+    if (selection == 'd')
+    {
+
+    }
 }
