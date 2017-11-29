0781dcf9929ee3813241c475e1911df667de31dc
Þorri Magnússon <thonnim@gmail.com>
2017-11-29 14:54:24 +0000

    Added implementation for ostream and istream in Extra

--- MainAssignment/MainAssignment/src/Extra.cpp
+++ MainAssignment/MainAssignment/src/Extra.cpp
@@ -5,7 +5,20 @@ Extra::Extra()
     //ctor
 }
 
+
 Extra::~Extra()
 {
     //dtor
+}
+
+
+ostream& operator << (ostream& out, const Extra& extra)
+{
+    return out;
+}
+
+
+istream& operator >> (istream& in, Extra& extra)
+{
+    return in;
 }
