1cb596da43217835b85cda48974adb9750932d94
Þorri Magnússon <thonnim@gmail.com>
2017-11-29 15:53:54 +0000

    Implemented two constructors and ostream and istream overloaders to Extra

--- MainAssignment/MainAssignment/include/Extra.h
+++ MainAssignment/MainAssignment/include/Extra.h
@@ -9,7 +9,16 @@ using namespace std;
 class Extra
 {
     public:
+
+        enum Type
+        {
+            Drink,
+            Sauce,
+            SideDish
+        };
+
         Extra();
+        Extra(string name, Extra::Type type, double price);
         virtual ~Extra();
 
         friend ostream& operator << (ostream& out, const Extra& extra);
@@ -18,6 +27,9 @@ class Extra
     protected:
 
     private:
+        char name[32];
+        double price;
+        Extra::Type type;
 };
 
 #endif // EXTRA_H
