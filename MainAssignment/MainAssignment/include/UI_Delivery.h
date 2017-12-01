91eec6c9734a2f543b82a1b5bfccc6fb3103b633
hjordissveinsd <hjordis16@ru.is>
2017-12-01 15:44:57 +0000

    lagaði UI_Salesman

--- MainAssignment/MainAssignment/include/UI_Delivery.h
+++ MainAssignment/MainAssignment/include/UI_Delivery.h
@@ -1,12 +1,17 @@
 #ifndef UI_DELIVERY_H
 #define UI_DELIVERY_H
-
+#include "DataManager.h"
+#include "topping.h"
+#include "pizza.h"
+#include "Extra.h"
+#include "Order.h"
 
 class UI_Delivery
 {
     public:
         UI_Delivery();
         virtual ~UI_Delivery();
+        void deliveryMenu();
     protected:
     private:
 };
