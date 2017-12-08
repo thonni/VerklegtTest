76112bc176cbfe723bd5935343caa79bf5590469
Sigudur Galdur <galdur97@hotmail.com>
2017-12-08 12:49:30 +0000

    Minor changes

--- MainAssignment/MainAssignment/src/UI_Delivery.cpp
+++ MainAssignment/MainAssignment/src/UI_Delivery.cpp
@@ -66,6 +66,9 @@ void UI_Delivery::deliveryMenu()
                 cin >> orderMark;
                 break;
             }
+
+            /// Here it would be kind of cool to get the "O" option only if the orer is to be home delivered. If it's picked up, only the D option would show.
+            /// Also, if it's picked up, they payment method choice would appear first, but if it's home delivered, thar option would appear after the order is market "on its way"
             cout << "Please choose a payment method." << endl;
             // card or cash.
 
@@ -89,24 +92,4 @@ void UI_Delivery::deliveryMenu()
             }
         }
     }
-
-    /*
-    /// Here it would be kind of cool to get the "O" option only if the orer is to be home delivered. If it's picked up, only the D option would show.
-    /// Also, if it's picked up, they payment method choice would appear first, but if it's home delivered, thar option would appear after the order is market "on its way"
-    cout << "Please choose a payment method." << endl;
-    // card or cash.
-
-    cout << "Please mark the order appropriately." << endl;
-    cout << "Input O when the order is on its way" << endl;
-    cout << "Input D when the order is delivered." << endl;
-    cout << "Choose B to go back." << endl;
-    cin >> orderMark;
-    if (orderMark == 'O')
-    {
-        /// The number of order they choose gets marked "on its way" in the file.
-    }
-    else if (orderMark == 'D')
-    {
-        /// The number of order they choose gets marked "delivered" in the file.
-    }*/
 }
